fa4aea20214d537709d78c3765ea9a2b6ecfcaa3
MacBook Air <macbookair@MacBook-Air-MacBook-4.local>
2025-05-05 12:38:37 +0500

    add

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,20 +12,16 @@ const About = () => {
                 <Grid templateColumns={{ base: "1fr", md: "1fr 1fr" }} gap={10}>
                     <GridItem>
                         <Heading as="h3" size={{ base: "md", lg: "lg" }} mb={4}>
-                            {t("ООО <<ELECTROMASH SERVICE>> оказывают спектр услуг:")}
+                            {t("Нашата История")}
                         </Heading>
                         <Divider width="80px" borderColor="red.500" mb={6} />
-                        <Text fontSize="lg" mb={4}>
-                            * Монтаж и капитальный ремонт высоковольтных трансформаторов
-                            от 35 кВ до 500 кВ <br />
-                            * Строительство распределительных сетей напряжением от 0,4 кВ до
-                            500 KB <br />
-                            * Такелажный работе: перемещения крупно габаритный и тяжелей
-                            оборудования <br />
-                            * Дегазация и очистка трансформаторного масло <br />
-                            * Вакуумирования трансформаторов <br />
-                            * Регенерация трансформаторного масло <br />
-                        </Text>
+                        <Text fontSize="lg" mb={4} 
+                        dangerouslySetInnerHTML={{
+                            __html: t("about_text1")
+                        }}
+                        />
+                            
+                    
                         {/* <Text fontSize="lg" mb={4}>
                             {t("about_text2")}
                         </Text>
