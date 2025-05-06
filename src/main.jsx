43ee5479c0aea1f682d7ba1d75f9bf1866f5a09f
MacBook Air <macbookair@MacBook-Air-MacBook-4.local>
2025-05-06 14:03:05 +0500

    add

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,13 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 
 import './i18n'
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ToastContainer />
     <ChakraProvider>
       <BrowserRouter>
         <App />
