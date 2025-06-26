4b6c5066eb1b1b2c70cd8d6415f3f3e397ec9a87
MacBook Air <macbookair@MacBook-Air-MacBook-4.local>
2025-06-26 09:59:15 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,8 @@
-import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
-import Header from './components/Header'
-import Services from './components/Services'
-import ContactPage from './pages/ContactPage'
-import About from './components/About'
-import ServicesAboutPage from './pages/ServicesAboutPage'
-import ProjectsPage from './pages/ProjectsPage'
-
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={
-          <>
-            <Header />
-            <About />
-            <Services />
-          </>
-        } />
-        <Route path='/contact' element={
-          <>
-            <ContactPage />
-          </>
-        } />
-        <Route path='/project' element={
-          <>
-            <ProjectsPage />
-          </>
-        } />
-        <Route path='/services/:id' element={<ServicesAboutPage />} />
-      </Routes>
-      <Footer />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'
-import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter } from 'react-router-dom';
 
 import './i18n'
-import { ToastContainer } from 'react-toastify';
-import "react-toastify/dist/ReactToastify.css";
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ToastContainer />
-    <ChakraProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
