cecc1c5c61bfc6040e06962ef18c313050381931
MacBook Air <macbookair@MacBook-Air-MacBook-4.local>
2025-06-30 12:39:07 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'Add commentMore actions
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
