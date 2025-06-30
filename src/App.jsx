88358959dd5e60d07a978b366da3ffaf2d01fd96
MacBook Air <macbookair@MacBook-Air-MacBook-4.local>
2025-06-30 12:38:19 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import { Route, Routes } from 'react-router-dom'Add commentMore actions
 import './App.css'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+import Services from './components/Services'
+import ContactPage from './pages/ContactPage'
+import About from './components/About'
+import ServicesAboutPage from './pages/ServicesAboutPage'
+import ProjectsPage from './pages/ProjectsPage'
 
 function App() {
   return (
     <>
+     <Navbar />Add commentMore actions
+      <Routes>
+        <Route path='/' element={
+          <>
+            <Header />
+            <About />
+            <Services />
+          </>
+        } />
+        <Route path='/contact' element={
+          <>
+            <ContactPage />
+          </>
+        } />
+        <Route path='/project' element={
+          <>
+            <ProjectsPage />
+          </>
+        } />
+        <Route path='/services/:id' element={<ServicesAboutPage />} />
+      </Routes>
+      <Footer />
     </>
   )
 }
